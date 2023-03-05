c10222aab941f1ba6650f5263de259cec34d2afe
shinto <shinto.philip007@gmail.com>
2023-03-05 13:30:39 +0100

    use typeorm to interact with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true, // Automatically load entities from the entities folder. This is a convenience feature, but it's not recommended in production.
+      synchronize: true, // Don't use this in production! This will drop and re-create your database every time the app is restarted.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
